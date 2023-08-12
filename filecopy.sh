0e30afbc8968452d53a79c5b1cd1330b366d6bf8
Takao Akaki <mongonta@gmail.com>
2023-08-12 14:46:53 +0000

    1st commit

---
+++ filecopy.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+
+sudo mv /external/julius_files.tar .
+
+echo Start a browser and open the following URL to download dictation-kit-v4.1.tar.gz.
+echo ブラウザを起動して、下記のURLを開いてdictation-kit-v4.1.tar.gzをダウンロードしてください。
+echo https://ja.osdn.net/dl/julius/dictation-kit-v4.1.tar.gz/
+echo SSL証明書が期限切れのようでダウンロードするときにプライバシーエラーが出ますので注意してください。2023/8/12に確認
